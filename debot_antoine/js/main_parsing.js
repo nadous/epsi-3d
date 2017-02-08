49e0dbeb631f1213018a6a298613b9cf1670a2ac
adebot <antoine.debot@epsi.fr>
2017-02-08 15:22:48 +0100

    including project

--- debot_antoine/js/main_parsing.js
+++ debot_antoine/js/main_parsing.js
@@ -2,24 +2,26 @@ import Papa from "node_modules/papaparse/papaparse"
 
 Papa.SCRIPT_PATH = "node_modules/papaparse/papaparse.js";
 
-function doStuff(data) {
-    console.log(data);
-}
+parseData("../../data/data_co2.csv", doStuff);
 
 function parseData(url, callBack) {
-    Papa.parse(url, {
-		worker: true,
+	Papa.parse(url, {
 		delimiter: ",",
 		header: true,
         download: true,
-        dynamicTyping: true,
-		step: function(results) {
-			//console.log("Row data:", results.data);
-		},
-        complete: function(results) {
-            callBack(results.data);
-        }
+		complete: function(results, file) {
+				//console.log("Finished:", results.data);
+			var result=[];
+			results.data.forEach(function (value) {
+			  console.log(value);
+			  result[value["Country Name"]] = value["2013"];
+			});
+			console.log(results.data);
+			callBack(result);
+		}
     });
 }
 
-parseData("../../data/data_co2.csv", doStuff);
+function doStuff(data) {
+    console.log(data);
+}
